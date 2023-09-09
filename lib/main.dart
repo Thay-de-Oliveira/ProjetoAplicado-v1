baff2e0110952babc992955a042175569db12700
User_1KK <thaina_l_oliveira@estudante.sc.senai.br>
2023-09-09 00:06:05 +0000

    Atualizando a main

--- lib/main.dart
+++ lib/main.dart
@@ -27,33 +27,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-/*class MyApp extends StatelessWidget {
-  @override //Sobrescreve o método build da classe pai
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Defesa Civil',
-      home: Scaffold(
-        appBar: PreferredSize(
-          //Define a barra superior
-          preferredSize: Size.fromHeight(65), //Altura
-          child: Menu(context), //Cria a barra
-        ),
-        body: MyHomePage(),
-        bottomNavigationBar:
-            BottomMenu(), //Passa o menu inferior através do "BottomMenu()"
-        backgroundColor: Color(0xFFF1F1F1), //Fundo
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    //Constrói a interface
-    return Center(
-      child: Text('Conteúdo da Página'),
-    );
-  }
-}*/
