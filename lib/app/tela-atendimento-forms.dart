3699a55a44bfe0e3bac3d7d4589cb20edff74c93
User_1KK <thaina_l_oliveira@estudante.sc.senai.br>
2023-09-16 23:06:45 +0000

    Adicionando últimos campos do formulario

--- lib/app/tela-atendimento-forms.dart
+++ lib/app/tela-atendimento-forms.dart
@@ -16,6 +16,19 @@ class _AtendimentoFormsState extends State<AtendimentoForms> {
   String _selectedVistoriaRealizada = 'Selecionar';
   String _selectedTipoRealizada = 'Selecionar';
   String _selectedDate = '';
+  String _selectedEntregarItens = 'Selecionar';
+
+  bool isAguaSelected = false;
+  bool isCestasSelected = false;
+  bool isKitsSelected = false;
+  bool isPastilhaSelected = false;
+  bool isColchaoSelected = false;
+  bool isLonaSelected = false;
+  bool isCumeeiraSelected = false;
+  bool isPregoParafusoSelected = false;
+  bool isTelhaSelected = false;
+  bool isReservatorioSelected = false;
+  bool isFraldaSelected = false;
 
   List<String> numberOptions = [
     'Selecionar protocolo',
@@ -43,11 +56,12 @@ class _AtendimentoFormsState extends State<AtendimentoForms> {
   ];
   List<String> vistoriaRealizadaOptions = ['Selecionar', 'Sim', 'Não'];
   List<String> tipoVistoriaOptions = ['Selecionar', 'Presencial', 'Remoto'];
+  List<String> entragarItensOptions = ['Selecionar', 'Sim', 'Não'];
 
   InputDecoration _customInputDecoration(String labelText) {
     return InputDecoration(
       labelText: labelText,
-      labelStyle: TextStyle(fontSize: 18), //Tamanho da fonte
+      labelStyle: TextStyle(fontSize: 16), //Tamanho da fonte dos campos
       border: OutlineInputBorder(
         borderRadius: BorderRadius.circular(10.0), //Borda arredondada
       ),
@@ -65,6 +79,7 @@ class _AtendimentoFormsState extends State<AtendimentoForms> {
 
   @override
   Widget build(BuildContext context) {
+    var isResponsavel1Selected;
     return Scaffold(
       appBar: null,
       body: SingleChildScrollView(
@@ -205,8 +220,8 @@ class _AtendimentoFormsState extends State<AtendimentoForms> {
                         SizedBox(width: 16), // Espaçamento entre os campos
                         Expanded(
                           child: TextFormField(
-                            decoration: _customInputDecoration(
-                                'Data e hora da vistoria:'),
+                            decoration:
+                                _customInputDecoration('Data da vistoria:'),
                           ),
                         ),
                       ],
@@ -214,23 +229,211 @@ class _AtendimentoFormsState extends State<AtendimentoForms> {
 
                     SizedBox(height: 20),
 
-                    Row(
-                      children: [
-                        Expanded(
-                          child: ElevatedButton.icon(
-                            onPressed: () {
-                              //Adicionar AQUI a lógica para anexar imagens, PDFs ou anexos
-                            },
-                            icon: Icon(Icons.attach_file), // Ícone de anexo
-                            label: Text('Anexar Arquivos'),
-                            style: ElevatedButton.styleFrom(
-                              primary: Colors.blue, // Cor do botão
-                              onPrimary: Colors.white, // Cor do texto do botão
-                            ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Registro da vistoria',
+                        labelStyle: TextStyle(fontSize: 16),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color:
+                                  Colors.grey), // Cor da borda quando inativo
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Colors.blue), // Cor da borda quando ativo
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        suffixIcon: Padding(
+                          padding: EdgeInsets.only(right: 16.0),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Container(
+                                width: 30,
+                                height: 30,
+                                child: Image.asset(
+                                    'assets/imagens/icon-baixar.png'),
+                              ),
+                              Container(
+                                height: 30,
+                                child: Image.asset(
+                                    'assets/imagens/icon-camera.png'),
+                              ),
+                            ],
                           ),
                         ),
-                      ],
+                      ),
                     ),
+
+                    SizedBox(height: 30),
+
+                    DropdownButtonFormField<String>(
+                      value: _selectedEntregarItens,
+                      items: entragarItensOptions.map((String option) {
+                        return DropdownMenuItem<String>(
+                          value: option,
+                          child: Text(option),
+                        );
+                      }).toList(),
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          _selectedCanalAtendimento = newValue!;
+                        });
+                      },
+                      decoration: _customInputDecoration(
+                          'Será entregue itens de assistencia humanitaria?'), // Aplicar estilo personalizado
+                    ),
+
+                    SizedBox(height: 30),
+
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                            color: Colors.grey), // Cor da borda quando inativo
+                        borderRadius:
+                            BorderRadius.circular(10.0), // Borda arredondada
+                      ),
+                      child: ExpansionTile(
+                        title: Row(
+                          children: [
+                            Text(
+                              'Quais itens foram entregues?',
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ],
+                        ),
+                        trailing: Icon(Icons.arrow_drop_down), //Ícone de seta
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                CheckboxListTile(
+                                  title: Text('Água Potável'),
+                                  value: isAguaSelected,
+                                  onChanged: (bool? newValue) {
+                                    setState(() {
+                                      isAguaSelected = newValue ?? false;
+                                    });
+                                  },
+                                ),
+                                CheckboxListTile(
+                                  title: Text('Cestas básicas'),
+                                  value: isCestasSelected,
+                                  onChanged: (bool? newValue) {
+                                    setState(() {
+                                      isCestasSelected = newValue ?? false;
+                                    });
+                                  },
+                                ),
+                                CheckboxListTile(
+                                  title: Text('Kits'),
+                                  value: isKitsSelected,
+                                  onChanged: (bool? newValue) {
+                                    setState(() {
+                                      isKitsSelected = newValue ?? false;
+                                    });
+                                  },
+                                ),
+                                CheckboxListTile(
+                                  title:
+                                      Text('Pastilha potabilizadora de água'),
+                                  value: isPastilhaSelected,
+                                  onChanged: (bool? newValue) {
+                                    setState(() {
+                                      isPastilhaSelected = newValue ?? false;
+                                    });
+                                  },
+                                ),
+                                CheckboxListTile(
+                                  title: Text('Colchões'),
+                                  value: isColchaoSelected,
+                                  onChanged: (bool? newValue) {
+                                    setState(() {
+                                      isColchaoSelected = newValue ?? false;
+                                    });
+                                  },
+                                ),
+                                CheckboxListTile(
+                                  title: Text('Lonas'),
+                                  value: isLonaSelected,
+                                  onChanged: (bool? newValue) {
+                                    setState(() {
+                                      isLonaSelected = newValue ?? false;
+                                    });
+                                  },
+                                ),
+                                CheckboxListTile(
+                                  title: Text('Cumeeiras'),
+                                  value: isCumeeiraSelected,
+                                  onChanged: (bool? newValue) {
+                                    setState(() {
+                                      isCumeeiraSelected = newValue ?? false;
+                                    });
+                                  },
+                                ),
+                                CheckboxListTile(
+                                  title: Text('Pregos/Parafusos'),
+                                  value: isPregoParafusoSelected,
+                                  onChanged: (bool? newValue) {
+                                    setState(() {
+                                      isPregoParafusoSelected =
+                                          newValue ?? false;
+                                    });
+                                  },
+                                ),
+                                CheckboxListTile(
+                                  title: Text('Telhas'),
+                                  value: isTelhaSelected,
+                                  onChanged: (bool? newValue) {
+                                    setState(() {
+                                      isTelhaSelected = newValue ?? false;
+                                    });
+                                  },
+                                ),
+                                CheckboxListTile(
+                                  title: Text('Reservatórios'),
+                                  value: isReservatorioSelected,
+                                  onChanged: (bool? newValue) {
+                                    setState(() {
+                                      isReservatorioSelected =
+                                          newValue ?? false;
+                                    });
+                                  },
+                                ),
+                                CheckboxListTile(
+                                  title: Text('Fraldas'),
+                                  value: isFraldaSelected,
+                                  onChanged: (bool? newValue) {
+                                    setState(() {
+                                      isFraldaSelected = newValue ?? false;
+                                    });
+                                  },
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    SizedBox(height: 30),
+
+                    TextField(
+                      decoration: _customInputDecoration('Observações'),
+                      maxLines: null, // Permite múltiplas linhas
+                      keyboardType: TextInputType
+                          .multiline, // Define o teclado como multilinha
+                      onChanged: (String value) {
+                        // Aqui você pode atualizar o estado com o valor do campo de observação
+                        // Exemplo: _observacoes = value;
+                      },
+                    )
                   ],
                 ),
               ),
